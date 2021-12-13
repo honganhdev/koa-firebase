7fb32649f1f438948118a9534c163172f0534cf1
H Anh <anhta.9910@gmail.com>
2021-12-13 13:09:34 +0700

    fix update

--- functions/src/handlers/db/managerData.js
+++ functions/src/handlers/db/managerData.js
@@ -15,33 +15,11 @@ async function updateDocuments({ collectionName, id, data }) {
   const ref = await db.collection(collectionName).doc(id);
   let dataUpdate;
   try {
-    const res = await db.runTransaction(async (t) => {
-      const doc = await t.get(ref);
-      await t.update(ref, data);
-      dataUpdate = doc.data();
-    });
-    return dataUpdate;
+    await ref.update(data);
   } catch (error) {
     console.log(error);
   }
 }
-
-// async function getDocument({ collectionName, id }) {
-//   const res = await db.collection(collectionName);
-//   const querySnapshot = await res.where("id", "==", id).get();
-//   if (querySnapshot.empty) {
-//     console.log("No matching documents.");
-//     return;
-//   }
-
-//   let delObj = [];
-
-//   querySnapshot.forEach((doc) => {
-//     delObj.push(doc.data());
-//     console.log(doc.data());
-//     return doc.data();
-//   });
-// }
 
 async function deleteDocument({ collectionName, id }) {
   // [START firestore_data_delete_doc]
